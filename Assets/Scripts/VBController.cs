d581696de894309a65bb4730781069a24d1c8173
Sophie Chen Yang <yangchensophie@gmail.com>
2021-11-07 19:27:32 +0100

    added in virtual buttons

---
+++ Assets/Scripts/VBController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VBController : MonoBehaviour
+{
+
+    public GameObject cube, capsule;
+    VirtualButtonBehaviour[] vrb;
+
+    void Start()
+    {
+        //cube.SetActive(true);
+        //capsule.SetActive(true);
+        vrb = GetComponentsInChildren<VirtualButtonBehaviour>();
+
+        for (int i = 0; i< vrb.Length; i++)
+        {
+            vrb[i].RegisterOnButtonPressed(onButtonPressed);
+            vrb[i].RegisterOnButtonReleased(onButtonReleased);
+        }
+        
+    }
+
+    void onButtonPressed(VirtualButtonBehaviour vb)
+    {
+        if(vb.VirtualButtonName == "OneVB")
+        {
+            cube.SetActive(true);
+            capsule.SetActive(false);
+        } else if (vb.VirtualButtonName == "TwoVB")
+        {
+            cube.SetActive(false);
+            capsule.SetActive(true);
+        } else
+        {
+            throw new UnityException(vb.VirtualButtonName + "virtual button not supported");
+        }
+        
+    }
+
+    void onButtonReleased(VirtualButtonBehaviour vb)
+    {
+        capsule.SetActive(false);
+        cube.SetActive(false);
+        Debug.Log("Button Released");
+
+    }
+}
